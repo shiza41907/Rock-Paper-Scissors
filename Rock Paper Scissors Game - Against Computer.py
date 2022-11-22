da2e891b8a871e990bd5d3b615d9f61d6aafec97
shiza41907 <114109815+shiza41907@users.noreply.github.com>
2022-11-22 14:06:01 -0700

    Computer action and coding for winning

--- Rock Paper Scissors Game - Against Computer.py
+++ Rock Paper Scissors Game - Against Computer.py
@@ -6,6 +6,22 @@ player_action = input("Choose from rock, paper, or scissors: ")
 import random
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
-
+if player_one ==  computer_action:
+    print("Tie!")
+elif player_one == "rock":
+    if computer_action == "scissors":
+        print("Player one wins!")
+    elif  computer_action == "paper":
+        print("You lost!")    
+elif player_one == "paper":
+    if  computer_action == "scissors":
+        print("You lost")
+    elif  computer_action == "rock":
+        print("Player one wins!")
+elif player_one == "paper":
+    if  computer_action == "rock":
+        print("Player one wins!")
+    elif  computer_action == "scissors":
+        print("You lost!")
 
 print(computer_action)
